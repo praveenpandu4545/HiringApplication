78a1e63390a436898795762a6ad7f035edc1762c
praveenpandu4545 <praveenprincegnt@gmail.com>
2026-01-19 19:51:37 +0530

    Removed Waste things in main class

--- src/main/java/com/praveen/HiringApplication.java
+++ src/main/java/com/praveen/HiringApplication.java
@@ -10,11 +10,6 @@ public class HiringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HiringApplication.class, args);
-		System.out.println("Hi praveen our app is working.....");
-		Student s1 = new Student();
-		s1.setName("praveen");
-		System.out.println(s1.getName());
-		System.out.println(s1.getDepartment());
 	}
 
 }
