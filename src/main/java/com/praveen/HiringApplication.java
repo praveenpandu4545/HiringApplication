04ea26a1c7d914f4d12523bcdb23127f1510912b
praveenpandu4545 <praveenprincegnt@gmail.com>
2026-01-20 12:00:33 +0530

    Added CorsConfig

--- src/main/java/com/praveen/HiringApplication.java
+++ src/main/java/com/praveen/HiringApplication.java
@@ -3,7 +3,6 @@ package com.praveen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.praveen.entities.Student;
 
 @SpringBootApplication
 public class HiringApplication {
--- src/main/java/com/praveen/controller/DriveController.java
+++ src/main/java/com/praveen/controller/DriveController.java
@@ -1,6 +1,7 @@
 package com.praveen.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,4 +21,14 @@ public class DriveController {
         Drive savedDrive = driveService.createDrive(request);
         return ResponseEntity.ok(savedDrive);
     }
+    
+    @PostMapping("/testing")
+    public ResponseEntity<?> upload(
+    	    @RequestParam("driveNumber") int driveNumber,
+    	    @RequestParam("file") MultipartFile file
+    	) {
+    	    System.out.println("drive: " + driveNumber);
+    	    System.out.println("file: " + file.getOriginalFilename());
+    	    return ResponseEntity.ok("Received");
+    	}
 }
